593061b4cb73aaf6e2d60b02299220f9fb1a58e2
vanflux <lucasabruzzi@hotmail.com>
2025-06-21 18:53:34 -0300

    feat: far distance on bomb scene

--- src/scenes/bomb/scene.ts
+++ src/scenes/bomb/scene.ts
@@ -3,7 +3,7 @@ import { Bomb } from "./bomb";
 
 export class BombScene extends Scene {
   init() {
-    this.root.position.z = 4;
+    this.root.position.z = 4.5;
     this.add(new Bomb());
   }
 }
